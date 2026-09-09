152b38e014343fce5353e246c5466c01c7ba206f
cobyzero8 <cobyzero8@hotmail.com>
2026-09-09 12:19:51 -0500

    Update Supabase URL and anon key in auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,6 +1,6 @@
 // Configuración de Supabase
-const SUPABASE_URL = 'https://TU-PROYECTO.supabase.co';
-const SUPABASE_ANON_KEY = 'TU-CLAVE-ANONIMA';
+const SUPABASE_URL = 'https://hicjmlcdnrcjriuqhppb.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhpY2ptbGNkbnJjanJpdXFocHBiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI5NDYxOTMsImV4cCI6MjA5ODUyMjE5M30.GIJbvJXKv-QkKx13xrImrUCf-kbRyOapW4QNA83qYb8';
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 // Verificar sesión al cargar la página
